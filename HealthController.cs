b3a8f2217df2afbb926199b9311561284940e6d2
Leo <37282804+niuniu6niuniu@users.noreply.github.com>
2019-03-31 13:50:59 +0800

    Add files via upload

---
+++ HealthController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    // access health variable
+    [SerializeField] private float health = 100f;
+    // apply damage function
+    public void ApplyDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
